0ee92c52e98c2c5468d05665c8e285a62102541c
Leandro Borges <leandrinho_216@hotmail.com>
2024-10-30 20:49:48 -0300

    Updates

--- src/redux/User/store.ts
+++
@@ -1,8 +0,0 @@
-import logger from 'redux-logger';
-import { rootReducer } from './root-reducer';
-import { configureStore } from '@reduxjs/toolkit';
-
-export const store = configureStore({
-	reducer: rootReducer,
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-});
